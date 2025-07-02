ccfce5f26a342fce23be634ff829878ea42853cc
xguimiranda <saguilherme16@gmail.com>
2025-07-02 15:18:38 -0300

    FEAT: Implementar função para verificar se números são pares ou ímpares

---
+++ parImpar.py
@@ -0,0 +1,9 @@
+def verificar_par_impar():
+    print("=== VERIFICADOR DE PAR OU ÍMPAR ===")
+    print("Digite números para verificar se são pares ou ímpares")
+    print("Digite 0 para sair do programa\n")
+    
+
+
+if __name__ == "__main__":
+    verificar_par_impar()
