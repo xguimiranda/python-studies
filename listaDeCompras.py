38dd83f25a057e058b917da020a36d6d8176ddf8
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-08 20:04:32 -0300

    FEAT: Update item deletion process and enhance user input validation

--- listaDeCompras.py
+++ listaDeCompras.py
@@ -4,23 +4,16 @@ lista = []
 
 while True:
     print("Selecione uma opção:")
-    selecao = input("[i]nserir [a]pagar [l]istar: ")
+    selecao = input("[i]nserir [a]pagar [l]istar [s]air: ")
 
-    if selecao not in ('i', 'a', 'l'):
+    if selecao not in ('i', 'a', 'l', 's'):
         print("selecione uma opção valida")
         continue
     
     if selecao == 'a':
-        indice = input("selecione um indice para apagar: ")
-        if not indice.isdigit():
-            print("Digite um numero válido!")
-            continue
-        indice = int(indice)
-        if indice not in range(len(lista)):
-            print("Não foi possivel apagar esse indice!")
-            continue
-        else:
-            lista.pop(indice)
+        try:
+            indice = int(input("selecione um indice para apagar: "))
+            del lista[indice]
             os.system('cls')
         except (ValueError):
             print("Digite um numero valido!")
