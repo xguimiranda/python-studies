a61d7cbf2c821084fe93aae12afebd7f91fb9897
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-10 21:19:45 -0300

    FEAT: Refactor CPF digit calculation naming

--- verificadorCPF.py
+++ verificadorCPF.py
@@ -1,12 +1,12 @@
 cpf = '74682489070'
 nove_digitos = cpf[:9]
-contador_regressivo = 10
+contador_regressivo_1 = 10
 
-resultado = 0
-for digito in nove_digitos:
-    resultado += int(digito) * contador_regressivo
-    contador_regressivo -= 1
+resultado_digito_1 = 0
+for digito_1 in nove_digitos:
+    resultado_digito_1 += int(digito_1) * contador_regressivo_1
+    contador_regressivo_1 -= 1
 
-digito = ((resultado * 10) % 11) 
-digito = digito if digito <= 9 else 0
-print(digito)
+digito_1 = ((resultado_digito_1 * 10) % 11) 
+digito_1 = digito_1 if digito_1 <= 9 else 0
+print(digito_1)
