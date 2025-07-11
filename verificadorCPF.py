87998dd261f9291ffd6a697ec7ce0646dfcaefcc
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-10 21:17:35 -0300

    FEAT: Refactor CPF verification logic to improve clarity and structure

--- verificadorCPF.py
+++ verificadorCPF.py
@@ -1,5 +1,12 @@
 cpf = '74682489070'
 nove_digitos = cpf[:9]
-contador_digito = 10
+contador_regressivo = 10
 
 resultado = 0
+for digito in nove_digitos:
+    resultado += int(digito) * contador_regressivo
+    contador_regressivo -= 1
+
+digito = ((resultado * 10) % 11) 
+digito = digito if digito <= 9 else 0
+print(digito)
