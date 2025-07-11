4a447f43c85af6469c675247129d4adcc19efcd3
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-10 21:26:09 -0300

    FEAT: Complete CPF digit calculation logic with second digit verification

--- verificadorCPF.py
+++ verificadorCPF.py
@@ -10,3 +10,13 @@ for digito_1 in nove_digitos:
 digito_1 = ((resultado_digito_1 * 10) % 11) 
 digito_1 = digito_1 if digito_1 <= 9 else 0
 print(digito_1)
+
+contador_regressivo_2 = 11
+resultado_digito_2 = digito_1
+for digito_2 in nove_digitos:
+    resultado_digito_2 += int(digito_2) * contador_regressivo_1
+    contador_regressivo_2 -= 1
+
+digito_2 = ((resultado_digito_2 * 10) % 11) 
+digito_2 = digito_2 if digito_2 <= 9 else 0
+print(digito_2)
