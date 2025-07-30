ff6ea7f4043e757034764a0a6c67d67de77c03da
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-30 20:28:59 -0300

    Fix multiplication logic and standardize function naming

--- exercicioFuncoes.py
+++ exercicioFuncoes.py
@@ -1,21 +1,20 @@
-
 def multiplicacao(*args):
     total = 1
     for numeros in args:
-        total *= args
+        total *= numeros
     return total
 
-def ParImpar(numero):
+def par_impar(numero):
     multiplo_de_dois = numero % 2 == 0
     if multiplo_de_dois:
         return f'{numero} é par'
     return f'{numero} é impar'
 
 print("***TESTE***")
-numeros = input("Digite números separados por espaço para serem multiplicados: ")
+numeros = input("Digite os números que deseja multiplicar, separados por espaço: ")
 numeros_lista = [int(n) for n in numeros.split()]
-print("Resultado da multiplicação: ", multiplicacao(numeros_lista))
+print("Resultado da multiplicação: ", multiplicacao(*numeros_lista))
 
 numero = int(input("Digite um número para ver se é par ou ímpar: "))
-print(ParImpar(numero))
+print(par_impar(numero))
 
