b6995bb7c31dfd86b713288f859f34f12a5803bf
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-30 20:49:27 -0300

    Fix indentation and standardize function calls in multiplication functions

--- criandofucoes.py
+++ criandofucoes.py
@@ -1,12 +1,12 @@
 def multiplicar(multiplicador):
-        def tipo_multiplicacao(numero):
-            return numero * multiplicador
+    def tipo_multiplicacao(numero):
+        return numero * multiplicador
     return tipo_multiplicacao
 
-    duplicar = multiplicar(2)
-    triplicar= multiplicador(3)
-    quadriplicar= multiplicador(4)
+duplicar = multiplicar(2)
+triplicar = multiplicar(3)
+quadriplicar = multiplicar(4)
 
-    print(duplicar(2))
-    print(triplicar(4))
-    print(quadriplicar(6))
+print(duplicar(2))
+print(triplicar(4))
+print(quadriplicar(6))
