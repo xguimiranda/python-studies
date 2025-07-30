ff249036b356565770185527c2dcacc19f5fefd5
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-30 20:46:10 -0300

    Implement multiplication functions for duplicating, tripling, and quadrupling numbers

---
+++ criandofucoes.py
@@ -0,0 +1,10 @@
+def multiplicar(multiplicador):
+    def tipo_multiplicacao(numero):
+       return numero * multiplicador
+    return tipo_multiplicacao
+    
+
+    funcao_duplicar = multiplicar(2)
+    funcao_triplicar= multiplicador(3)
+    funcao_quadriplicar= multiplicador(4)
+
