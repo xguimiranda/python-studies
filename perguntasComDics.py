4d09abde48bf21d734a74e48f24e612212d54d2e
Guilherme Miranda <saguilherme16@gmail.com>
2025-08-02 10:19:47 -0300

    Refactor print statements and variable names for consistency in quiz system

--- perguntasComDics.py
+++ perguntasComDics.py
@@ -21,33 +21,37 @@ perguntas = [
 
 qtd_acertos = 0
 for pergunta in perguntas:
-    print("pergunta:",pergunta['Pergunta'])
+    print('Pergunta:', pergunta['Pergunta'])
     print()
 
-    opções = pergunta['Opções']
-    for i, opção in enumerate(opções):
-        print(f'{i})', opção)
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
     print()
 
-    escolha = input("Escolha uma opção: ")
+    escolha = input('Escolha uma opção: ')
 
     acertou = False
     escolha_int = None
-    qtd_opções = len(opções)
+    qtd_opcoes = len(opcoes)
 
     if escolha.isdigit():
         escolha_int = int(escolha)
 
     if escolha_int is not None:
-        if escolha_int >= 0 and escolha_int < qtd_opções:
-            if opções[escolha_int] == pergunta['Resposta']:
-                acertou == True
-
-    if acertou:
-        qtd_acertos += 1
-        print("acertou👍")
-    else:
-        print("Errou❌")
-
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
 
     print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
+    else:
+        print('Errou ❌')
+
+    print()
+
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
