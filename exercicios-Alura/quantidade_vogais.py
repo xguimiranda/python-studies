11d0c88de83da2c4aebaccaf24f79f5f2ec5adda
Guilherme Miranda <saguilherme16@gmail.com>
2025-09-27 19:09:47 -0300

    ADD: implement function to count vowels in a given text

---
+++ exercicios-Alura/quantidade_vogais.py
@@ -0,0 +1,12 @@
+def contar_vogais(texto):
+    vogais = "aeiou"
+    quantidade = 0
+
+    for letra in texto.lower():
+        if letra in vogais:
+            quantidade += 1
+
+    return quantidade
+
+texto = input("Digite um texto: ")
+print(f"O texto contém {contar_vogais(texto)} vogais.")
